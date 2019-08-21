e916104611c07db6e26456d88a3495498479a86d
shubham <shubhambendre96@gmail.com>
2019-08-21 20:24:01 +0530

    template programmable android

---
+++ OpenGL/Android/04_Prog_Template/app/src/main/java/com/AMC/template/GLESView.java
@@ -0,0 +1,160 @@
+package com.AMC.template;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLES31;
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.egl.EGLConfig;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.graphics.Color;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.OnDoubleTapListener;
+
+public class GLESView extends GLSurfaceView implements GLSurfaceView.Renderer ,OnDoubleTapListener,OnGestureListener
+{
+	private GestureDetector gestureDetector;
+	private final Context context;
+	
+	public GLESView(Context drawingContext)
+	{
+		super(drawingContext);
+		
+		context = drawingContext;
+		//
+		gestureDetector = new GestureDetector(drawingContext, this, null, false);
+		//drawingContext:it's like global enviroment 
+		//this:who is going to handle gesture
+		//null:other is not going to handle
+		//false:internally used by android
+		gestureDetector.setOnDoubleTapListener(this);
+		
+		setEGLContextClientVersion(3);
+		
+		setRenderer(this);
+		
+		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+	}
+	
+	//methods OnDoubleTapListner
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		int eventaction = event.getAction(); //not needed
+		if(!gestureDetector.onTouchEvent(event))
+			super.onTouchEvent(event);
+		return(true);
+	}
+	
+	 @Override 
+	 public boolean onDoubleTap(MotionEvent event)
+	 {
+		
+		 return(true);
+	 }
+	 
+	 @Override 
+	 public boolean onDoubleTapEvent(MotionEvent event)
+	 {
+		 
+		 return(true);
+	 }
+	 
+	 @Override
+	 public boolean onSingleTapConfirmed(MotionEvent event)
+	 {
+		 
+		 return(true);
+	 }
+	 
+	 
+	 //On GestureListner
+	 @Override
+	 public boolean onDown(MotionEvent event)
+	 {
+		 return(true);
+		 
+	 }
+	 //
+	 @Override
+	 public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+	 {
+		 return(true);
+	 }
+	 
+	 @Override
+	 public void onLongPress(MotionEvent e)
+	 {
+		 
+		 
+	 }
+	 
+	 @Override 
+	 public boolean onScroll(MotionEvent e1, MotionEvent e2, float disX, float disY)
+	 {
+		 
+		 return(true);
+	 }
+	 
+	 @Override 
+	 public void onShowPress(MotionEvent e)
+	 {
+		 
+	 }
+	 
+	 @Override
+	 public boolean onSingleTapUp(MotionEvent e)
+	 {
+		 return(true);
+	 }
+	 //implements GLSurfaceView.Renderer
+	 @Override
+	 public void onSurfaceCreated(GL10 gl,EGLConfig config)
+	 {
+		 String version = gl.glGetString(GL10.GL_VERSION);
+		 
+		 System.out.println("RTR: " + version);
+		 
+		 initialize();
+		 System.out.println("RTR: After Initialize()");
+	 }
+	 @Override
+	 public void onSurfaceChanged(GL10 unused, int width, int height)
+	 {
+		 System.out.println("RTR: onSurfaceChanged()");
+		 resize(width, height);
+	 }
+	 
+	 @Override
+	 public void onDrawFrame(GL10 unused)
+	 {
+		 display();
+		 System.out.println("RTR: onDrawFrame()");
+	 }
+	 
+	 //our custom methods
+	
+	 private void initialize()
+	 {
+		 System.out.println("RTR: In start Initialize()");
+		 GLES31.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+		 System.out.println("RTR: End  Initialize()");
+	 }
+		
+	private void resize(int width, int height)
+	{
+		GLES31.glViewport(0, 0, width, height);
+		System.out.println("RTR: In resize()");
+	}
+	
+	private void display()
+	{
+		System.out.println("RTR: Start of display()");
+		GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT | GLES31.GL_DEPTH_BUFFER_BIT);
+		
+		requestRender();
+		System.out.println("RTR: End of display()");
+	}
+}
